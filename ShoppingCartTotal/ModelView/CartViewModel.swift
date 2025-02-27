bc5d1ab797411820f6b35f094aae202ad16c07aa
Julien Hwang <jhwang@lcs.on.ca>
2025-02-27 09:25:39 -0500

    Made the view model for the app that blocks non-numeric results

--- ShoppingCartTotal/ModelView/CartViewModel.swift
+++ ShoppingCartTotal/ModelView/CartViewModel.swift
@@ -5,3 +5,48 @@
 //  Created by Julien Hwang on 27/2/2025.
 //
 
+import Foundation
+
+@Observable
+class CartViewModel {
+    
+    //MARK: Stored properties
+    var providedItem1: String
+    var providedItem2: String
+    var providedItem3: String
+    var recoverySuggestion: String = ""
+    
+    //MARK: Computed Properties
+    var shoppingCartTotal: ShoppingCartTotal? {
+        guard let item1 = Double(providedItem1) else {
+            recoverySuggestion = "Please provide a numeric value for item 1"
+            
+            return nil
+        }
+        
+        guard let item2 = Double(providedItem2) else {
+            recoverySuggestion = "Please provide a numeric value for item 2"
+            return nil
+        }
+        
+        guard let item3 = Double(providedItem3) else {
+            recoverySuggestion = "Please prvide a numeric value for item3"
+            return nil
+        }
+        
+        
+        recoverySuggestion = ""
+        return ShoppingCartTotal(item1: item1, item2: item2, item3: item3)
+    }
+    init(providedItem1: String,
+         providedItem2: String,
+         providedItem3: String,
+         recoverySuggestion: String
+    ) {
+        self.providedItem1 = providedItem1
+        self.providedItem2 = providedItem2
+        self.providedItem3 = providedItem3
+        self.recoverySuggestion = recoverySuggestion
+    }
+    
+}
