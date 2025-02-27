ec292b629fcce6c863a90f3d2243fbd7c0bc90eb
Julien Hwang <jhwang@lcs.on.ca>
2025-02-27 08:50:54 -0500

    Initial Commit

---
+++ ShoppingCartTotal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingCartTotal
+//
+//  Created by Julien Hwang on 27/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingCartTotal/ShoppingCartTotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartTotalApp.swift
+//  ShoppingCartTotal
+//
+//  Created by Julien Hwang on 27/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartTotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
