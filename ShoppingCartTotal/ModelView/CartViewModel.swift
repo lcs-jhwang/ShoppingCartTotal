db2b62ca5ffd20aae0e0b2314389a6dadd857129
Julien Hwang <jhwang@lcs.on.ca>
2025-02-27 09:07:50 -0500

    Added the model into the app

---
+++ ShoppingCartTotal/ModelView/CartViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  CartViewModel.swift
+//  ShoppingCartTotal
+//
+//  Created by Julien Hwang on 27/2/2025.
+//
+
---
+++ ShoppingCartTotal/Models/ShoppingCartTotal.swift
@@ -0,0 +1,26 @@
+//
+//  ShoppingCartTotal.swift
+//  ShoppingCartTotal
+//
+//  Created by Julien Hwang on 27/2/2025.
+//
+
+import Foundation
+
+struct ShoppingCartTotal {
+    //MARK: Stored properties
+    var item1: Double
+    var item2: Double
+    var item3: Double
+    
+    var total: Double {
+        
+        var solution: Double = 0.0
+        solution = item1 + item2 + item3
+        return solution
+    }
+    
+    var hst: Double {
+        return total * 0.13
+    }
+}
